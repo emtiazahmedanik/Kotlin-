208b25689808f2ecdc6750a96e1b7114783415ad
Emtiaz ahmed <98808241+emtiazahmedanik@users.noreply.github.com>
2023-10-02 20:50:53 +0600

    datatypes

---
+++ 03_DataType_Ex03.kt
@@ -0,0 +1,13 @@
+fun main(){
+   var K : Int = 20
+//Int , Float, Double, String , Char
+  var Choose : String
+  Choose = "What you like more"
+    println(Choose +" "+ K)
+    var d : Double = 34535534.54333433
+    var f : Float = 4635.4495F
+    var c : Char = 'A'
+
+
+
+}
