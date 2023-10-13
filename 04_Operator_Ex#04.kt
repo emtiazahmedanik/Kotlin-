6282df241c4e542289f5aec2ccddb1617e81fbbe
Emtiaz ahmed <98808241+emtiazahmedanik@users.noreply.github.com>
2023-10-13 10:37:23 +0600

    operator in kotlin

---
+++ 04_Operator_Ex#04.kt
@@ -0,0 +1,43 @@
+fun main(args:Array<String>){
+ /* var a = 30 ; var b = 20
+    println("a + b = "+(a+b))
+    println("a - b = "+(a-b))
+    println("a * b = "+(a*b))
+    println("a /b = "+(a/b))   // Arithmatic operation
+
+    var x = 10
+    var y = 5
+
+    println("X is graeter than y : "+(x>y))
+    println("X is less than y : "+(x<y))
+    println("X is graeter than equal y : "+(x>=y))
+    println("X is less than equal y : "+(x<=y))
+    println("X is equal to  y : "+(x==y))
+    print("X is not equal to y : "+(x!=y))      //Relational operator
+
+    //Assignment operator
+  var f = 20
+  var g = 10
+  //f = f+g
+  f+=g
+  g-=f
+  println(f)
+  println(g)
+  */
+
+
+  //Unary operator
+
+  //increment
+  var n = 5
+  print("Increment :"+(++n))
+  println("Increment :"+(n++))
+  println(n)
+  //Decrement
+  var Number = 40
+  println(Number--)
+  println(Number)
+
+
+
+}
