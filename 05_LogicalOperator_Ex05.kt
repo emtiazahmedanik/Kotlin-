dbfcd806f50aefdaa3fa87a35c79603a946b89b8
Emtiaz ahmed <98808241+emtiazahmedanik@users.noreply.github.com>
2023-10-13 10:37:54 +0600

    logical operator

---
+++ 05_LogicalOperator_Ex05.kt
@@ -0,0 +1,14 @@
+fun main(){
+   // And operator &&
+    var age18 = true
+    var hasidcard = false
+    var canvote = age18 && hasidcard
+    println(canvote)
+
+    // Or operator ||
+    var hasmoney = false
+    var hascar = false
+    var cango = hasmoney || hascar
+    print(cango)
+
+}
