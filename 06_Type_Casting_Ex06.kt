8c8e57e775f317dc62e8ad41a0b86c1c4904d069
Emtiaz ahmed <98808241+emtiazahmedanik@users.noreply.github.com>
2023-10-13 10:38:18 +0600

    type casting

---
+++ 06_Type_Casting_Ex06.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("240 converted to byte : "+(240.toByte()))
+    println("5000 to short :"+(5000.toShort()))
+    println("10L - long to Integer :"+(10L.toInt()))
+    println("20 -Integer to float :"+(20.toFloat()))
+    println("65 - Integer to character :"+(65.toChar()))
+
+}
