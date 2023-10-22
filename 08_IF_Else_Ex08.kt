8c42e7afe7b06a569d0bf5be912aa6bbffc826af
Emtiaz ahmed <98808241+emtiazahmedanik@users.noreply.github.com>
2023-10-22 21:11:09 +0600

    IF Else Condition

---
+++ 08_IF_Else_Ex08.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val x = 0
+    if(x>0){
+        print("X is positive")
+    }
+    else if(x==0){
+        print("X is zero")
+    }
+    else{
+        print("X is negative")
+    }
+}
